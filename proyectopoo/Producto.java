bcb3f7f65b5de8e118b68db428665cabab3bbca2
Ramirox64 <50385725+Ramirox64@users.noreply.github.com>
2020-03-19 22:51:16 -0500

    Proyecto Final

---
+++ proyectopoo/Producto.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    private String nombre;
+    private int precio;
+    private int cantidad;
+    private String tipo;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public int getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the cantidad
+     */
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public Producto(String nombre, int precio,int cantidad,String tipo){
+        this.nombre=nombre;
+        this.precio=precio;
+        this.cantidad=cantidad;
+        this.tipo=tipo;
+        
+        
+        
+    }
+    public Producto(int cantidad, int precio){
+        this.cantidad=cantidad;
+        this.precio=precio;
+        
+    }
+    
+}
